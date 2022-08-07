60c2642cb26b236084e1eaad4882535d487ad567
omer-yusufoglu <omer.yusufoglu@vodafone.com>
2022-08-07 22:10:48 +0300

    add validation for mandatory parameters

--- src/main/java/com/qardio/temperature/controller/TemperatureController.java
+++ src/main/java/com/qardio/temperature/controller/TemperatureController.java
@@ -8,6 +8,7 @@ import com.qardio.temperature.service.TemperatureService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -23,6 +27,7 @@ import java.util.List;
  */
 @Tag(name = "Temperature-Controller", description = "Temperature Rest API")
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/api/v1/temperature")
 public class TemperatureController {
@@ -34,14 +39,14 @@ public class TemperatureController {
     }
 
     @PostMapping
-    public List<TemperatureRecord> saveTemperatureRecord(@RequestBody List<TemperatureRecord> temperatureRecordList) {
+    public List<TemperatureRecord> saveTemperatureRecord(@RequestBody @NotEmpty(message = "Input temperatureRecord list cannot be empty.") List<@Valid TemperatureRecord> temperatureRecordList) {
         return temperatureService.saveTemperatureRecord(temperatureRecordList);
     }
 
     @GetMapping
-    public List<AggregatedTemperature> getAggregatedTemperature(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDateTime,
-                                                                @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDateTime,
-                                                                @RequestParam FrequencyType frequencyType) {
+    public List<AggregatedTemperature> getAggregatedTemperature(@NotNull @RequestParam  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDateTime,
+                                                                @NotNull @RequestParam  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDateTime,
+                                                                @NotNull @RequestParam  FrequencyType frequencyType) {
         return temperatureService.getAggregatedTemperatureByCriteria(startDateTime, endDateTime, frequencyType);
     }
 
